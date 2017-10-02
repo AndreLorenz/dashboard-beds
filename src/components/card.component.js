e37d5f0929cca059d2b82d4334e738e36de12f9c
André <andre.lorenz@philips.com>
2017-10-02 18:43:55 -0300

    New

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -15,69 +15,231 @@ class CardComponent {
     //   this.cards.push({ number, title: `A000${number}`, patientName: `João ${number}`, content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.  `, footer: `Philips Clinical Informatics` }));
 
     this.cards = [{
-      "bedInfo": {
-        "statusColor": "#33333",
-        "description": "A 00001",
-        "bedStatus": [{
-            "type": "accessibility"
+        "bedInfo": {
+          "statusColor": "#33333",
+          "description": "A 000000000000000000000000000000000000000001",
+          "bedStatus": [{
+              "type": "accessibility"
+            },
+            {
+              "type": "male"
+            }
+          ]
+        },
+        "contentInfo": {
+          "status": "PATIENT",
+          "riskClassification": this.getRiskColor("DANGER"),
+          "patientName": "Randy Barldon",
+          "encounterNumber": 92121,
+          "physicianName": "Dr. Joel Munoz",
+          "moreInfo": [{
+              "label": "label",
+              "description": "description",
+              "display": true
+            },
+            {
+              "label": "label2",
+              "description": "description2",
+              "display": false
+
+            }, {
+              "label": "label3",
+              "description": "description3",
+              "display": true
+            }
+          ],
+          "patientStatus": [{
+              "type": "VIP",
+              "image": require("./../assets/male.svg")
+            },
+            {
+              "type": "Male",
+              "image": require("./../assets/male.svg")
+            },
+            {
+              "type": "Alergic",
+              "moreInfo": "Cilic, Diabetic",
+              "image": require("./../assets/allergies-warning.svg")
+            }
+          ]
+        },
+        "encounterInfos": [{
+            "type": "BACTERIA_TEST",
+            "image": require("./../assets/patient-isolated-precaution.svg")
           },
           {
-            "type": "male"
+            "type": "TEST",
+            "image": require("./../assets/patient-isolated-grey.svg")
           },
           {
-            "type": "waiting"
+            "type": "SUSPENDED_ITEM",
+            "image": require("./../assets/prescription-clock.svg")
+          },
+          {
+            "type": "ALLERGY",
+            "image": require("./../assets/prescription-clock-yellow.svg")
+          },
+          {
+            "type": "BIRTHDAY",
+            "image": require("./../assets/cake.svg")
+          }
+        ]
+      }, {
+        "bedInfo": {
+          "statusColor": "#33333",
+          "description": "A 000000000000000000000000000000000000000001",
+          "bedStatus": [{
+              "type": "accessibility"
+            },
+            {
+              "type": "male"
+            }
+          ]
+        },
+        "contentInfo": {
+          "status": "PATIENT",
+          "riskClassification": this.getRiskColor("DANGER"),
+          "patientName": "Randy Barldon",
+          "encounterNumber": 92121,
+          "physicianName": "Dr. Joel Munoz",
+          "moreInfo": [{
+              "label": "label",
+              "description": "description",
+              "display": true
+            },
+            {
+              "label": "label2",
+              "description": "description2",
+              "display": false
+
+            }, {
+              "label": "label3",
+              "description": "description3",
+              "display": true
+            }
+          ],
+          "patientStatus": [{
+              "type": "VIP",
+              "image": require("./../assets/male.svg")
+            },
+            {
+              "type": "Male",
+              "image": require("./../assets/male.svg")
+            },
+            {
+              "type": "Alergic",
+              "moreInfo": "Cilic, Diabetic",
+              "image": require("./../assets/allergies-warning.svg")
+            }
+          ]
+        },
+        "encounterInfos": [{
+            "type": "BACTERIA_TEST",
+            "image": require("./../assets/patient-isolated-precaution.svg")
+          },
+          {
+            "type": "TEST",
+            "image": require("./../assets/patient-isolated-grey.svg")
+          },
+          {
+            "type": "SUSPENDED_ITEM",
+            "image": require("./../assets/prescription-clock.svg")
+          },
+          {
+            "type": "ALLERGY",
+            "image": require("./../assets/prescription-clock-yellow.svg")
+          },
+          {
+            "type": "BIRTHDAY",
+            "image": require("./../assets/cake.svg")
+          }
+          ,
+          {
+            "type": "BIRTHDAY",
+            "image": require("./../assets/cake.svg")
           }
         ]
       },
-      "contentInfo": {
-        "status": "PATIENT",
-        "riskClassification": "CRITICAL",
-        "patientName": "Randy Barldon",
-        "encounterNumber": 92121,
-        "physicianName": "Dr. Joel Munoz",
-        "moreInfo": [{
-            "label": "label",
-            "description": "description"
-          },
-          {
-            "label": "label2",
-            "description": "description2"
-          }
-        ],
-        "patientStatus": [{
-            "type": "VIP"
-          },
-          {
-            "type": "Male"
-          },
-          {
-            "type": "Alergic",
-            "moreInfo": "Cilic, Diabetic"
-          }
-        ]
-      },
-      "encounterInfo": [{
-          "type": "BACTERIA_TEST"
+      {
+        "bedInfo": {
+          "statusColor": "#33333",
+          "description": "A 00004",
+          "bedStatus": []
         },
-        {
-          "type": "TEST"
-        },
-        {
-          "type": "SUSPENDED_ITEM"
-        },
-        {
-          "type": "ALLERGY"
-        },
-        {
-          "type": "BIRTHDAY"
-        },
-        {
-          "type": "DISCHARGE"
+        "contentInfo": {
+          "status": "PATIENT",
+          "riskClassification": this.getRiskColor("SAFETY"),
+          "patientName": "Rose Salazar",
+          "encounterNumber": 50297,
+          "physicianName": "Dra. Leila Meyer",
+          "patientStatus": [{
+              "type": "ALLERGY",
+              "image": require("./../assets/allergies-warning.svg")
+            },
+            {
+              "type": "FEMALE",
+              "image": require("./../assets/female.svg")
+            }
+          ]
         }
-      ]
-    }]
+      },
+      {
+        "bedInfo": {
+          "statusColor": "#33333",
+          "description": "A 00008",
+          "bedStatus": [{
+              "type": "accessibility"
+            },
+            {
+              "type": "male"
+            }
+          ]
+        },
+        "contentInfo": {
+          "status": "AVAILABLE",
+          "title": "Available",
+          "moreInfo": [{
+              "label": "Reserved",
+              "description": "05-12-2017",
+              "display": true
+            },
+            {
+              "label": "Encounter",
+              "description": "2236549",
+              "display": true
+
+            }
+          ]
+        }
+      }
+
+    ];
+
+    this.cards.forEach(card => {
+      if (card.encounterInfos && card.encounterInfos.length > 5) {
+        card.encounterInfos.limitTo = 4;
+        card.encounterInfos.remainingInfos = card.encounterInfos.length - 4;
+      }
+    });
+    console.log(this.cards);
+  }
 
 
+  getRiskColor(color) {
+    switch (color) {
+      case "NOTICE":
+        return "#008EE8"
+      case "SAFETY":
+        return "#3FB763"
+      case "CAUTION":
+        return "#FFDB1F"
+      case "WARNING":
+        return "#FF9514"
+      case "DANGER":
+        return "#FF5454"
+      default:
+        return false;
+    }
   }
 
   drag(event) {
